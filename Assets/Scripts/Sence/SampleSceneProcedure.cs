d64b3a9890905c04ae43db7ac9ec9eda343a132b
lee cheng <nixiaozi01@hotmail.com>
2020-06-14 18:47:25 +0800

    init 修复和确认了资源数据的生成功能 自定义流程的初始化

---
+++ Assets/Scripts/Sence/SampleSceneProcedure.cs
@@ -0,0 +1,101 @@
+﻿using GameFramework.Event;
+using System.Collections.Generic;
+using UnityGameFramework.Runtime;
+
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+namespace GFRobbie
+{
+    public class SampleSceneProcedure : ProcedureBase
+    {
+        protected override void OnEnter(ProcedureOwner procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
+
+
+            GameEntry.Event.Subscribe(LoadSceneSuccessEventArgs.EventId, OnLoadSceneSuccess);
+            GameEntry.Event.Subscribe(LoadSceneFailureEventArgs.EventId, OnLoadSceneFailure);
+            GameEntry.Event.Subscribe(LoadSceneUpdateEventArgs.EventId, OnLoadSceneUpdate);
+            GameEntry.Event.Subscribe(LoadSceneDependencyAssetEventArgs.EventId, OnLoadSceneDependencyAsset);
+
+            GameEntry.Scene.LoadScene("Assets/Scenes/SampleScene.unity", 100, this);
+
+
+        }
+
+
+        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
+        {
+            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+
+            
+        }
+
+        protected override void OnLeave(ProcedureOwner procedureOwner, bool isShutdown)
+        {
+            GameEntry.Event.Unsubscribe(LoadSceneSuccessEventArgs.EventId, OnLoadSceneSuccess);
+            GameEntry.Event.Unsubscribe(LoadSceneFailureEventArgs.EventId, OnLoadSceneFailure);
+            GameEntry.Event.Unsubscribe(LoadSceneUpdateEventArgs.EventId, OnLoadSceneUpdate);
+            GameEntry.Event.Unsubscribe(LoadSceneDependencyAssetEventArgs.EventId, OnLoadSceneDependencyAsset);
+
+            base.OnLeave(procedureOwner, isShutdown);
+        }
+
+
+        private void OnLoadSceneSuccess(object sender, GameEventArgs e)
+        {
+            LoadSceneSuccessEventArgs ne = (LoadSceneSuccessEventArgs)e;
+            if (ne.UserData != this)
+            {
+                return;
+            }
+
+            Log.Info("Load scene '{0}' OK.", ne.SceneAssetName);
+
+            // 判断游戏状态，并且加载音乐
+            /*if (m_BackgroundMusicId > 0)
+            {
+                // GameEntry.Sound.PlayMusic(m_BackgroundMusicId);
+            }
+
+            m_IsChangeSceneComplete = true;*/
+        }
+
+        private void OnLoadSceneFailure(object sender, GameEventArgs e)
+        {
+            LoadSceneFailureEventArgs ne = (LoadSceneFailureEventArgs)e;
+            if (ne.UserData != this)
+            {
+                return;
+            }
+
+            Log.Error("Load scene '{0}' failure, error message '{1}'.", ne.SceneAssetName, ne.ErrorMessage);
+        }
+
+        private void OnLoadSceneUpdate(object sender, GameEventArgs e)
+        {
+            LoadSceneUpdateEventArgs ne = (LoadSceneUpdateEventArgs)e;
+            if (ne.UserData != this)
+            {
+                return;
+            }
+
+            Log.Info("Load scene '{0}' update, progress '{1}'.", ne.SceneAssetName, ne.Progress.ToString("P2"));
+        }
+
+        private void OnLoadSceneDependencyAsset(object sender, GameEventArgs e)
+        {
+            LoadSceneDependencyAssetEventArgs ne = (LoadSceneDependencyAssetEventArgs)e;
+            if (ne.UserData != this)
+            {
+                return;
+            }
+
+            Log.Info("Load scene '{0}' dependency asset '{1}', count '{2}/{3}'.", ne.SceneAssetName, ne.DependencyAssetName, ne.LoadedCount.ToString(), ne.TotalCount.ToString());
+        }
+
+
+    }
+
+
+}
